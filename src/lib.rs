68f4868e9b935d74a70e5e120c0764db1631f86e
Logan Keenan <d.logankeenan@gmail.com>
2022-02-14 07:49:27 -0600

    consume javascrit-adapter as public package

--- src/lib.rs
+++ src/lib.rs
@@ -65,7 +65,7 @@ impl<State: Clone + Send + Sync + 'static> Middleware<State> for HtmlInjection {
 const HTML_INJECTION: &str = r#"
 <script type='module'>
   import initializeWasm, {app, JsRequest, JsResponse} from '/out/notes_demo_spa.js';
-  import * as jsAdapter from '/node_modules/javascript-adapter/dist/index.js'
+  import * as jsAdapter from '/node_modules/@rora/javascript-adapter/dist/index.js'
 
   async function start() {
     await initializeWasm();
