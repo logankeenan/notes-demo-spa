70750e34bf22aca6e3c81efb0e2da641632656ed
Logan Keenan <d.logankeenan@gmail.com>
2022-02-16 21:19:45 -0600

    update @rora/javascript-adapter

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
-import { default as wasmbin }  from '../wasm-out/notes_demo_spa_bg.wasm';
+import {default as wasmbin} from '../wasm-out/notes_demo_spa_bg.wasm';
 import init, {app, JsRequest, JsResponse} from '../wasm-out/notes_demo_spa';
 import * as jsAdapter from "@rora/javascript-adapter"
 
-async function initialize() {
+(async () => {
     await init(wasmbin);
 
-    jsAdapter.initialize({
+    window.roraAdapter = jsAdapter.create({
         app,
         JsRequest,
         JsResponse
     });
-    jsAdapter.registerEvents();
-}
-
-window.rora = {};
-window.rora.javascriptAdapter = {};
-window.rora.javascriptAdapter.initialize = initialize;
-window.rora.javascriptAdapter.pageLoad = jsAdapter.pageLoad;
+    await window.roraAdapter.start();
+})();
